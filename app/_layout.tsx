a31c1ae0daa81159368fe52b33d3982eaa24f573
Sourav Sharma <dev.souravsharma@gmail.com>
2025-02-24 01:01:18 +0530

    code added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="signIn" options={{ headerShown: false }} />
+        <Stack.Screen name="signUp" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
